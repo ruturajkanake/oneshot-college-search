378ce6b5573777e2fb6c88d8a9af7760986e5ba6
Ruturaj Kanake <ruturajkanake1999@gmail.com>
2021-04-24 06:15:51 +0530

    fix: updated cors

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = ['http://localhost:3000', 'https://ruturaj-college-search.herokuapp.com', 'http://ruturaj-college-search.herokuapp.com'];
 
 app.use(
     cors({
